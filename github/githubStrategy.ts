01d38ef660e8a6eb193098e4f2fe52e9f9706331
Alvin Cheng <alvincheng50@gmail.com>
2021-02-24 12:09:53 -0500

    githubStrategy.ts - removed console logs and edited comments and spacing

--- github/githubStrategy.ts
+++ github/githubStrategy.ts
@@ -64,7 +64,7 @@ export default class GitHubStrategy {
     }
 
     // removes the '&' we just placed at the end of the string
-    if(paramString[paramString.length - 1] === '&'){
+    if (paramString[paramString.length - 1] === '&') {
       paramString = paramString.slice(0, -1);
     }
 
@@ -87,128 +87,109 @@ export default class GitHubStrategy {
     }
     const toReplaceArray: string[] = Object.keys(decodedString);
 
-    for(let i = 0; i < toReplaceArray.length; i++) {
+    for (let i = 0; i < toReplaceArray.length; i++) {
       while (encodedCode.includes(toReplaceArray[i])) {
         encodedCode = encodedCode.replace(toReplaceArray[i], decodedString[toReplaceArray[i]]);
       }
     }
+
     return encodedCode; 
   }
 
-  //*************************************/
   // ENTRY POINT
   async router(ctx: OakContext, next: Function) {
-    // DEBUGGING: 
-    // console.log('0.9 url returned from auth request in ctx.request.url.search:', ctx.request.url.search)
     // GO_Step 1 Request Permission
-    if(!ctx.request.url.search) return await this.authorize(ctx, next);
+    if (!ctx.request.url.search) return await this.authorize(ctx, next);
     // GO_Step 3 Exchange code for Token
-    // ?ACTION REQUIRED: verify that a successful response from getAuthToken includes 'code' in the location specified below
-    
-    if(ctx.request.url.search.slice(1, 5)=== 'code') {
-      // console.log("1.0 line 116 we got the code!");
-      return this.getAuthToken(ctx, next)};
-    // if(ctx.request.url.search.slice) -- error
-  }
-  
+    // ACTION REQUIRED: verify that a successful response from getAuthToken includes 'code' in the location specified below
+    if (ctx.request.url.search.slice(1, 5) === 'code') return this.getAuthToken(ctx, next)
+  };
+
   // STEP 1: sends the programatically constructed uri to an oauth 2.0 server
   async authorize(ctx: OakContext, next: Function) {
-    console.log("1.0 we are In authorize!");
-    // ! this
     return await ctx.response.redirect(this.authURL + this.uriFromParams);                   
   }
-  
-  //**************** getAuthToken *********************/
-  // STEP 2: /what happen before client choose/client choose yes or no
-  // STEP 3: /who handles it?/handle oauth 2.0 server response containing auth code
+
+  // STEP 2: client choose yes or no
+
+  // STEP 3: handle oauth 2.0 server response containing auth code
   // STEP 3.5: request access token in exchange for auth code
   async getAuthToken(ctx: OakContext, next: Function) {
-    // the URI send back to the endpoint you provided in step 1
+    // the URI send back to the endpoint provided in step 1
     const OGURI: string = ctx.request.url.search;
 
     if (OGURI.includes('error')) {
+      ////////////////////////////////////////////////
       // do error handling
       console.log('broke the code again');
     }
 
-   
-    // ACTION REQUIRED: verify that this function works for the format of the response you received. uncomment the line below to test:
-      // console.log('authorize response: ', OGURI);
+    // ACTION REQUIRED: verify that this function works for the format of the response received. uncomment the line below to test:
+      // console.log('AUTH RESPONSE:', OGURI);
     // splits the string at the '=,' storing the first part in URI1[0] and the part we want in URI1[1]
     let URI1: string[] = OGURI.split('=');
     // splits the string at the ampersand(&), storing the string with the access_token in URI2[0] 
     // and the other parameters at URI2[n]
     const URI2: string[] = URI1[1].split('&');
-    // Debug // console.log('uri on line 99', URI2[0])
     // PARSE THE URI
     const code: string = this.parseCode(URI2[0]);
-    // Debug // console.log("1.5 UR1:", URI1);
-    // Debug // console.log("1.5 UR2:", URI2);
-    console.log("1.5 the code is:", code);
-    // STEP 3.5
-    // ACTION REQUIRED: add or remove the parameters needed to send your token request
 
+    // STEP 3.5
+    // ACTION REQUIRED: add or remove the parameters needed to send as response to token request
     const tokenOptions: Options = {
       client_id: this.options.client_id,
       client_secret: this.options.client_secret,
-      code: code,
       redirect_uri: this.options.redirect_uri,
+      code: code,
     }
 
     // SEND A FETCH REQ FOR TOKEN
     try {
-      // console.log('2.0 url-sent-to-request-token, tokenOptions are: ', tokenOptions)
-      const options:any = {
+      const options: any = {
         method: 'POST',
         headers: {
           'Accept': 'application/json',
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify(tokenOptions
-        )
+        body: JSON.stringify(tokenOptions)
       };
 
       let data: any = await fetch('https://github.com/login/oauth/access_token', options);
-      // console.log('2.1 Line 177 Github data status', data.status);
       data = await data.json();
-      if (data.type === 'oAuthException') return console.log('token request threw oauth exception')
-      // console.log('2.1 line178', data);
 
-      // DEBUGGING 
+      //////////////////////////////
       if (data.type === 'oAuthException') return console.log('token request threw oauth exception')
+
       // PASSES TOKEN ON TO STEP 4
       return this.getAuthData(data);
     } catch(err) {
+      ////////////////////////////////////
       // console.log('2.3 Line 207 Fetch Req For Token Error MESSAGE : '+ err)
     }
   }
-
-  //-------------------------------- getAuthData --------------------------------//
 
   // STEP 4 get the access token from the returned data
   // STEP 4.5 exchange access token for user info
   async getAuthData(parsed: TokenData){ 
     // ACTION REQUIRED: 
-      // fill in the fields for tokenData based on the token obj you got back in the last step
-      // authData is what we're going to be passing back to dashport.ts
+      // fill in the fields for tokenData based on the token obj obtained in the last step
+      // authData is what is going to be passed back to dashport's authenticate method
     const authData: AuthData = {
       tokenData: {
         access_token: parsed.access_token,
         scope: parsed.scope,
         token_type: parsed.token_type,
-        }
+      },
+      userInfo: {
+        provider: '',
+        providerUserId: ''
+      }
     }
 
     // STEP 4.5: request user info
     // ACTION REQUIRED: 
-      // fill in the fields for auth options with whatever information is required by your OAuth service
-      // authOptions constructs the uri for your final fetch request
-      // const authOptions: any = {
-      //  input_token: authData.tokenData.access_token,
-    //   access_token: this.options.client_id + '|' + this.options.client_secret
-    // };
-    
-    // DEBUGGING: console.log('uri being used line 137', 'this.authDataUrl' + this.constructURI(authOptions))
+      // fill in the fields for auth options with the information required by requested OAuth service
+      // authOptions constructs the uri for the final fetch request
     try {
       const authOptions: any = {
         headers: {
@@ -217,9 +198,9 @@ export default class GitHubStrategy {
       };
       let data: any = await fetch('https://api.github.com/user', authOptions);
       data = await data.json();
-      console.log('auth data returned: ', data);
+
       // ACTION REQUIRED:
-        // Add whatever data you requested and want to pass back to dashport.ts here
+        // Add any data to pass back to dashport's authenticate method here
       authData.userInfo = {
         provider: this.name,
         providerUserId: data.id,
@@ -232,6 +213,7 @@ export default class GitHubStrategy {
 
       return authData;
     } catch(err) {
+      //////////////////////////////////////////////////
       console.log('3.4 L.252 getAuthData fetch error', err);
     }
   }
