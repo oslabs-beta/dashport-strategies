971de5e5d996b555cee9f640377ef907b5aa76cc
Alvin Cheng <alvincheng50@gmail.com>
2021-02-24 12:01:11 -0500

    facebookStrategy.ts - edited spacing

--- github/githubStrategy.ts
+++ github/githubStrategy.ts
@@ -1,27 +1,20 @@
-// import { AuthData, Options, TokenData } from '../facebook/types';
 import { OakContext, Options, AuthData, TokenData } from '../types.ts';
-// import { FetchOptions } from './types';
 
 /** 
  * Create an instance of 'GitHubStrategy'.
  *
- * * Options:
+ * Options:
  *
  *   - client_id: string                  Required
  *   - redirect_uri: string               Required
  *   - client_secret: string              Required
  *
- *                                     reference: https://docs.github.com/en/developers/apps/authorizing-oauth-apps
-*
-* 
-*
-*
-*/
-
+ * reference: https://docs.github.com/en/developers/apps/authorizing-oauth-apps
+ */
 export default class GitHubStrategy {
   name: string = 'github'
-  options: Options; // the options makes up the uriFromParams
-  uriFromParams: string; // wh: does github 
+  options: Options;
+  uriFromParams: string;
   authURL: string;
   tokenURL: string;
   authDataURL: string;
@@ -33,10 +26,12 @@ export default class GitHubStrategy {
   constructor (options: Options) {
     // customize required/desired field for the first redirect
     if (!options.client_id || !options.redirect_uri || !options.client_secret) {
+      //////////////////////////////////////////////////
       throw new Error('Mssing Required arguments');
     }
     
     this.options = options; 
+    //////////////////////////////////////////////
     // ACTION NEEDED:
       // input url for first endpoint from service below: 
     this.authURL = 'https://github.com/login/oauth/authorize?'
@@ -53,8 +48,8 @@ export default class GitHubStrategy {
     this.uriFromParams = this.constructURI(this.options);
   } 
 
-  //*************************************/
-  constructURI(options:Options, skip?:string[]): any{
+  constructURI(options: Options, skip?: string[]): any {
+    //////////////////////////
     let paramArray: string[][] = Object.entries(options);
     let paramString: string = '';
 
@@ -67,17 +62,17 @@ export default class GitHubStrategy {
       // adds the value and '&' for every member of options not in the skip array
       paramString += (value + '&');
     }
+
     // removes the '&' we just placed at the end of the string
     if(paramString[paramString.length - 1] === '&'){
       paramString = paramString.slice(0, -1);
     }
-    // console.log("0.5 paramString:", paramString);
+
     return paramString;
   }
      
-  // parseCode func parses an encoded URI  
+  // parses an encoded URI  
   parseCode(encodedCode: string): string {
-    
     const decodedString: { [name: string] : string } = {
       "%24": "$",
       "%26": "&",
@@ -90,9 +85,8 @@ export default class GitHubStrategy {
       "%3F": "?",
       "%40": "@"
     }
-
     const toReplaceArray: string[] = Object.keys(decodedString);
-    // console.log('encoded code (fb 172)', encodedCode);
+
     for(let i = 0; i < toReplaceArray.length; i++) {
       while (encodedCode.includes(toReplaceArray[i])) {
         encodedCode = encodedCode.replace(toReplaceArray[i], decodedString[toReplaceArray[i]]);
