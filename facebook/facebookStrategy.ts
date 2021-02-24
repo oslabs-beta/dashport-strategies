245f9de7c8dc15c8eadf1e86b6eaed69b896a152
Alvin Cheng <alvincheng50@gmail.com>
2021-02-24 11:49:39 -0500

    facebookStrategy.ts - edited catch error handling in getAuthData

--- facebook/facebookStrategy.ts
+++ facebook/facebookStrategy.ts
@@ -190,8 +190,7 @@ export default class FacebookStrategy {
 
       return authData;
     } catch(err) {
-      ////////////////////////////////////////////////////
-      console.log('getAuthData error on line 153 of Facebook', err);
+      return new Error(`ERROR in getAuthData: Unable to obtain auth data - ${err}`);
     }
   }
 }
